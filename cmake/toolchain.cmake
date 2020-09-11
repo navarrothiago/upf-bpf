5d139326511968929305db67e0506ab9f66b70d5
Thiago Navarro <navarro.ime@gmail.com>
2020-09-11 14:16:10 -0300

    [#15] Adds support of the AddressSanitizer

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -10,7 +10,7 @@ set(CMAKE_CXX_COMPILER_TARGET ${triple})
 
 # TODO Remove -target bpf
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -target bpf")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall ${CXX_ISYSTEM_DIRS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall ${CXX_ISYSTEM_DIRS} -fsanitize=address -O1 -fno-omit-frame-pointer" )
 
 set(CMAKE_OBJCOPY llvm-objcopy CACHE INTERNAL "objcopy tool")
 set(CMAKE_SIZE_UTIL llvm-size CACHE INTERNAL "size tool")
