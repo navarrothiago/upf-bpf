d9d49edef071903dab9191a0d0f97983dddc9276
Thiago Navarro <navarro.ime@gmail.com>
2021-02-18 21:25:18 -0300

    [#27] Adds traffic and platform configuration profiles

---
+++ tests/trex/traffic/gtp_1pkt_simple.py
@@ -0,0 +1,25 @@
+from trex_stl_lib.api import *
+from scapy.contrib.gtp import *
+
+class STLS1(object):
+
+    def create_stream (self):
+        return STLStream(
+            packet =
+                    STLPktBuilder(
+                        pkt = Ether()/IP(src="172.20.16.99",dst="192.168.15.12")/
+                                UDP(dport=2152)/GTP_U_Header(teid=100)/IP(src="10.10.10.10",dst="172.20.16.55",version=4)/UDP()/Raw('x'*20)
+                    ),
+            mode = STLTXCont())
+
+    def get_streams (self, direction = 0, **kwargs):
+        # create 1 stream
+        return [ self.create_stream() ]
+
+
+# dynamic load - used for trex console or simulator
+def register():
+    return STLS1()
+
+
+
