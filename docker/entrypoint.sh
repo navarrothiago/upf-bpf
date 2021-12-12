fc1aced866ce8f32d60b974c267fc93307460846
Thiago Navarro <navarro.ime@gmail.com>
2021-12-12 06:51:00 -0300

    fixes unknown host for dut and trex server in container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -20,20 +20,9 @@ ConfigureSSH() {
     ssh-keyscan github.com >> ${SSH_HOME_DIR}/known_hosts
     ssh-keyscan gitlab.com >> ${SSH_HOME_DIR}/known_hosts
 
-    #If the variables for the SSH jump server are defined, then we'll get the host keys from the DUT
-    # and TRex servers to it.
-    if  [ -n "$JUMP_SERVER_IP" ] && [ -n "$JUMP_SERVER_USERNAME" ] && [ -n "$JUMP_SERVER_PORT" ] && \
-        [ -n "$DUT_SERVER_IP" ] && [ -n "$TREX_SERVER_IP" ]; then
-        ssh-keyscan -p ${JUMP_SERVER_PORT} ${JUMP_SERVER_IP} >> ${SSH_HOME_DIR}/known_hosts
-
-        JUMP_SERVER_SSH_CMD="ssh ${JUMP_SERVER_USERNAME}@${JUMP_SERVER_IP} -p ${JUMP_SERVER_PORT}"
-        $JUMP_SERVER_SSH_CMD ssh-keyscan ${DUT_SERVER_IP} >> ${SSH_HOME_DIR}/known_hosts
-        $JUMP_SERVER_SSH_CMD ssh-keyscan ${TREX_SERVER_IP} >> ${SSH_HOME_DIR}/known_hosts
-    fi
-
     #If the secrets are defined, them they will be copied to the root user ssh folder
     # so we can set the correct permissions and ownership
-    if [ -f ${SECRETS_FOLDER}/SSH_PRIV_KEY ]; then 
+    if [ -f ${SECRETS_FOLDER}/SSH_PRIV_KEY ]; then
         cp ${SECRETS_FOLDER}/SSH_PRIV_KEY "${SSH_HOME_DIR}"/${SSH_PRIVATE_KEY_FILE} && \
         export SSH_PRIVATE_KEY_FILE="${SSH_HOME_DIR}/${SSH_PRIVATE_KEY_FILE}"
     fi
@@ -50,6 +39,20 @@ ConfigureSSH() {
     chown -R ${USERNAME}:${USERNAME} "${USERHOME}"
     chmod -R 770 ${USERHOME}
     chmod -R 600 ${SSH_HOME_DIR}/*
+
+    #If the variables for the SSH jump server are defined, then we'll get the host keys from the DUT
+    # and TRex servers to it.
+    echo "checking variables..."
+    if  [ -n "$JUMP_SERVER_IP" ] && [ -n "$JUMP_SERVER_USERNAME" ] && [ -n "$JUMP_SERVER_PORT" ] && \
+        [ -n "$DUT_IP" ] && [ -n "$TREX_SERVER_IP" ]; then
+        echo "generating ssh know host for jump server, dut and trex server..."
+        ssh-keyscan -p ${JUMP_SERVER_PORT} ${JUMP_SERVER_IP} >> ${SSH_HOME_DIR}/known_hosts
+
+        JUMP_SERVER_SSH_CMD="ssh ${JUMP_SERVER_USERNAME}@${JUMP_SERVER_IP} -p ${JUMP_SERVER_PORT}"
+        $JUMP_SERVER_SSH_CMD ssh-keyscan ${DUT_IP} >> ${SSH_HOME_DIR}/known_hosts
+        $JUMP_SERVER_SSH_CMD ssh-keyscan ${TREX_SERVER_IP} >> ${SSH_HOME_DIR}/known_hosts
+        echo "done"
+    fi
 }
 
 #Function to configure the .gitconfig file for the user
