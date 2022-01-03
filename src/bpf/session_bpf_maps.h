530e5f3b8117907fd9e86a239a7d65b825b8096a
Thiago Navarro <navarro.ime@gmail.com>
2022-01-02 22:04:15 -0300

    bpf: removes unsed maps in pfcp session program

--- src/bpf/session_bpf_maps.h
+++ src/bpf/session_bpf_maps.h
@@ -12,12 +12,6 @@
 #define SESSION_PDRS_MAX_SIZE 10
 #define SESSION_FARS_MAX_SIZE 10
 
-struct bpf_map_def SEC("maps") m_pdrs = {
-	.type        = BPF_MAP_TYPE_HASH,
-	.key_size    = sizeof(u16), // pdr_id
-	.value_size  = sizeof(pfcp_pdr_t_), // pdr
-	.max_entries = SESSION_PDRS_MAX_SIZE,
-};
 struct bpf_map_def SEC("maps") m_fars = {
 	.type        = BPF_MAP_TYPE_HASH,
 	.key_size    = sizeof(u32), // teid
@@ -31,19 +25,12 @@ struct bpf_map_def SEC("maps") m_redirect_interfaces = {
 	.max_entries = 10,
 };
 // Uplink map.
-struct bpf_map_def SEC("maps") m_teid_pdrs_counter = {
-	.type        = BPF_MAP_TYPE_HASH,
-	.key_size    = sizeof(teid_t_), // teid
-	.value_size  = sizeof(u32), // number of allocated PDR in teid
-	.max_entries = 100000,
-};
-// Uplink map.
 // TODO navarrothiago - Store multiple PDR.
 struct bpf_map_def SEC("maps") m_teid_pdr = {
 	.type        = BPF_MAP_TYPE_HASH,
 	.key_size    = sizeof(teid_t_), // teid
 	.value_size  = sizeof(pfcp_pdr_t_), // assuming only one PDR
-	.max_entries = 100000,
+	.max_entries = 10,
 };
 // Downlink map.
 // TODO navarrothiago - Store multiple PDR.
@@ -51,9 +38,8 @@ struct bpf_map_def SEC("maps") m_ueip_pdr = {
 	.type        = BPF_MAP_TYPE_HASH,
 	.key_size    = sizeof(u32), // UE IP address
 	.value_size  = sizeof(pfcp_pdr_t_), // assuming only one PDR
-	.max_entries = 100000,
+	.max_entries = 10,
 };
-
 // Static ARP Table. Used to get the MAC address of the next hop.
 // TODO navarrothiago - pinned this maps. It not depend on the session program
 struct bpf_map_def SEC("maps") m_arp_table = {
